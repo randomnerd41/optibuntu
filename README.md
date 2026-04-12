af1e46a8fbc5f743d3eda25f3312576d93a1f3b3
classiccatlinux <classiccatlinux@gmail.com>
2026-04-12 12:43:18 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,38 @@
+# Optibuntu
+**A free and Open source program**
+that makes ubuntu and ubuntu based systems
+faster!
+
+# verson 1.0_beta!
+
+# READ THIS!
+
+**FOR UBUNTU OR UBUNTU BASED DISTROS ONLY!**
+
+if youre on a non-gnome
+based ubuntu distro then
+say no when the program will ask:
+disable animations? (y/n):.
+    
+this program will disable snap!
+you will no longer be able to install
+programs with snap and programs
+installed with snap will not work.
+
+this program will also use sudo
+and apt to install other programs.
+
+programs used:
+preload
+
+**this program is in beta use at your own risk!**
+
+# info
+verson 1.0
+made by: classiccatlinux (randomnerd41)
+
+this program may only boost performance
+by a tiny bit!
+
+
+    
---
+++ main.py
@@ -0,0 +1,64 @@
+import os
+import time
+import sys
+
+print("Welcome to optibuntu - a free and open soucre")
+print("program for ubuntu (and ubuntu based systems)")
+print("that makes your system faster!")
+time.sleep(6)
+print("Please read the README.md file!")
+print("This program will remove snap!")
+print("And is for ubuntu based linux distros ONLY!")
+time.sleep(5)
+os.system('clear')
+in1 = input("start? (y/n): ")
+if in1 == "n":
+    print("exiting...")
+    time.sleep(1)
+    exit()
+elif in1 == "y":
+    os.system("sudo systemctl disable snapd && sudo systemctl stop snapd")
+    os.system("sudo apt purge snapd -y && rm -rf ~/snap")
+    os.system("sudo rm -rf /snap /var/snap /var/lib/snapd")
+    os.system("sudo apt-mark hold snapd")
+    os.system('clear')
+    
+else:
+    print("you cant do that!")
+    time.sleep(1)
+    exit()
+    
+in2 = input("would you like to turn off bluetooth and printer support? (y/n): ")
+
+if in2 == "y":
+    os.system("sudo systemctl disable bluetooth && sudo systemctl disable cups")
+    
+elif in2 == "n":
+    print("Will not disable bluetooth and printing")
+    time.sleep(2)
+    os.system('clear')
+    
+else:
+    print("you cant do that!")
+    time.sleep(1)
+    exit()
+    
+in3 = input("would you like to disable animations? (y/n): ")
+
+if in3 == "y":
+    os.system("gsettings set org.gnome.desktop.interface enable-animations false")
+    
+elif in3 == "n":
+    print("Will not disable animations")
+    time.sleep(2)
+    os.system('clear')
+    
+else:
+    print("you cant do that!")
+    time.sleep(1)
+    exit()
+    
+os.system("sudo apt install preload -y && echo 'vm.swappiness=10' | sudo tee -a /etc/sysctl.conf") 
+    
+  
+
