98367059abd26919c20fc535ef73429d9dbfbd27
classiccatlinux <classiccatlinux@gmail.com>
2026-05-12 20:52:47 -0400

    Add files via upload

---
+++ optibuntu-2.0-buged/runme.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# ===========
+# == runme ==
+# ===========
+
+# Made by classiccatlinux (randomnerd41)
+# AGPL 3.0 - FOSS
+# Version 1.0 
+
+sudo apt update -y  && sudo apt install python3 -y
+clear
+python3 main.py
+
