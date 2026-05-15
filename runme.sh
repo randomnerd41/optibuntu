7dfa2cf1f1a4dd021eab188ea0a19b59f5b9e801
classiccatlinux <classiccatlinux@gmail.com>
2026-05-15 15:37:47 -0400

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -6,9 +6,10 @@
 
 # Made by classiccatlinux (randomnerd41)
 # AGPL 3.0 - FOSS
-# Version 1.0 
+# Version 1.1 
 
+echo "Installing Python3 and running Optibuntu..."
+sleep 3
 sudo apt update -y  && sudo apt install python3 -y
 clear
 python3 main.py
-
